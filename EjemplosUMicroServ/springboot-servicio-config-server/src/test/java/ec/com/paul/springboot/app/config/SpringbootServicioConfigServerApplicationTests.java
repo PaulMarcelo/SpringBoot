8caf6c2dad5ab65818427210d9d2252514fb8087
Paul Yaguachi <yaguachi.marcelo@gmail.com>
2020-06-28 19:49:27 -0500

    agrega server config

---
+++ EjemplosUMicroServ/springboot-servicio-config-server/src/test/java/ec/com/paul/springboot/app/config/SpringbootServicioConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.paul.springboot.app.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicioConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- EjemplosUMicroServ/springboot-servicio-item/src/main/java/ec/com/paul/springboot/app/item/controller/ItemController.java
+++ EjemplosUMicroServ/springboot-servicio-item/src/main/java/ec/com/paul/springboot/app/item/controller/ItemController.java
@@ -1,9 +1,17 @@
 package ec.com.paul.springboot.app.item.controller;
 
+import java.util.HashMap;
 import java.util.List;
-
+import java.util.Map;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,34 +22,64 @@ import ec.com.paul.springboot.app.item.models.Item;
 import ec.com.paul.springboot.app.item.models.Producto;
 import ec.com.paul.springboot.app.item.models.service.IItemService;
 
+@RefreshScope
 @RestController
 public class ItemController {
+	
+	private static Logger log = LoggerFactory.getLogger(ItemController.class);
+	
+	@Autowired
+	private Environment env;
+	
 	@Autowired
 	@Qualifier("serviceFeign")
 	// @Qualifier("serviceRestTemplate")
 	private IItemService itemService;
+
+	@Value("${configuracion.texto}")
+	private String texto;
 
 	@GetMapping("/listar")
 	public List<Item> listar() {
 		return itemService.findAll();
 	}
 
-	@HystrixCommand(fallbackMethod="metodoAlternativo")
+	@HystrixCommand(fallbackMethod = "metodoAlternativo")
 	@GetMapping("/ver/{id}/cantidad/{cantidad}")
 	public Item detalle(@PathVariable Long id, @PathVariable Integer cantidad) {
 		return itemService.findById(id, cantidad);
 	}
-	
+
 	public Item metodoAlternativo(Long id, Integer cantidad) {
 		Item item = new Item();
 		Producto producto = new Producto();
-		
+
 		item.setCantidad(cantidad);
 		producto.setId(id);
 		producto.setNombre("Camara Sony");
 		producto.setPrecio(500.00);
 		item.setProducto(producto);
-		return item;	
+		return item;
+	}
+
+	
+//	public ResponseEntity<?> obtenerConfig() {
+	@GetMapping("/obtener-config")
+	public ResponseEntity<?> obtenerConfig(@Value("${server.port}") String puerto) {
+		log.info(texto);
+		Map<String, String> json = new HashMap<>();
+		json.put("texto", texto);
+		json.put("puerto", puerto);
+		
+		
+		if(env.getActiveProfiles().length>0 && env.getActiveProfiles()[0].equals("dev")) {
+			json.put("autor.nombre", env.getProperty("configuracion.autor.nombre"));
+			json.put("autor.email", env.getProperty("configuracion.autor.email"));
+		}
+		
+		
+		return new ResponseEntity<Map<String, String>>(json, HttpStatus.OK);
+
 	}
 
 }
